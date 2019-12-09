d982a29849d271d2808692cd8df132d923d82ee0
hifromkatie <50120761+hifromkatie@users.noreply.github.com>
2019-12-09 18:34:42 +0000

    Add files via upload
    Day1 Complete

--- Day1.c
+++ Day1.c
@@ -7,7 +7,7 @@ int main()
 	int mass_array[100];
 	int total_fuel=0;
 	int fuel;
-	
+	int cont;
 	input_mass =fopen("D1_mass.txt","r");
 	
 	if (input_mass==NULL)
@@ -18,10 +18,24 @@ int main()
 		fscanf(input_mass, "%d", &mass_array[x]);
 	}
 	
+//	fclose(input_mass);	
+	
 	for (x=0; x<100; x++)
 	{
-		fuel=(mass_array[x]/3)-2;
-		total_fuel=total_fuel+fuel;
+		fuel=mass_array[x];
+		cont=1;
+		while(cont==1)
+		{
+			fuel=(fuel/3)-2;
+			if (fuel>0)
+			{
+				total_fuel=total_fuel+fuel;
+			}
+			else
+			{
+				cont=0;
+			}
+		}
 	}
 	printf ("%d",total_fuel);
 	return(0);
